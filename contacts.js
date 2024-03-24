c564e9c9bcbb8eadd73f070da096ea4486e45f11
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-03-24 23:32:18 +0100

    almost finally v

--- contacts.js
+++ contacts.js
@@ -1,16 +1,78 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
-const contactsPath = path.parse('./db/contacts.json');
-console.log(contactsPath);
+const { v4: uuidv4 } = require('uuid');
+const colors = require('colors');
+const contactsPath = path.join(
+  __dirname,
+  '../goit-node-hw1/db',
+  'contacts.json'
+);
 
-function listContacts() {
-  fs.readFile(contactsPath);
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+  } catch (error) {
+    console.log('Error: '.red, error.message);
+  }
 }
-function getContactById() {}
-function removeContact() {}
-function addContact() {}
+async function getContactById(id) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const items = JSON.parse(data);
+    items.find(item => {
+      if (item.id === id) {
+        console.table(item);
+      }
+    });
+  } catch (error) {
+    console.log('Error: '.red, error.message);
+  }
+}
+
+async function removeContact(id) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contactToRemove = contacts.find(contact => contact.id === id);
+    if (!contactToRemove) {
+      console.log(`Contact with id ${id} not found...`.yellow);
+      return;
+    }
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    fs.writeFile(contactsPath, JSON.stringify(updatedContacts), error => {
+      if (error) {
+        console.log('Error: '.red, error.message);
+      }
+    });
+    console.table(contacts);
+    console.log(`Contact with id: ${id} successfully removed!`.green);
+  } catch (error) {
+    console.log('Error: ', error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = { id: uuidv4(), name, email, phone };
+    const newContacts = [...contacts, contact];
+    fs.writeFile(contactsPath, JSON.stringify(newContacts), error => {
+      console.log(error);
+    });
+    console.table(newContacts);
+    console.log(`Contact ${name} successfully added!`.green);
+  } catch (error) {
+    console.log('Error: ', error.message);
+  }
+}
 
 module.exports = {
   contactsPath,
   listContacts,
+  getContactById,
+  removeContact,
+  addContact,
 };
--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const contacts = require('./contacts');
 const { Command } = require('commander');
+
 const command = new Command();
-command.option('-a, -action <type>', 'choose action');
+
+command
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'username')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+command.parse(process.argv);
+
+const argv = command.opts();
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      contacts.listContacts();
+      break;
+    case 'get': {
+      contacts.getContactById(id);
+      break;
+    }
+    case 'remove': {
+      contacts.removeContact(id);
+      break;
+    }
+    case 'add': {
+      contacts.addContact(name, email, phone);
+      break;
+    }
+  }
+}
+invokeAction(argv);
